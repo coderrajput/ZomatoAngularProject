3b0f0d5ccbf1b29ef2f2856acefd04eda2e1dd2c
coderrajput <coderrajput@gmail.com>
2020-03-19 00:50:53 +0530

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SearchComponentComponent } from './search-component/search-component.component';
+import { SearchComponent } from './search/search.component';
+import { CitySelectionComponent } from './city-selection/city-selection.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponentComponent,
+    SearchComponent,
+    CitySelectionComponent
   ],
   imports: [
     BrowserModule
